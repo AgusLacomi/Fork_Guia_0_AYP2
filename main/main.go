69453cfb3b2cce9a0f1f49ee661741de8928b44e
AgusLacomi <aguslacomi@gmail.com>
2023-03-16 12:51:39 -0300

    Punto 2 Finalizado

--- main/main.go
+++ main/main.go
@@ -7,21 +7,20 @@ import (
 // By AgusLacomi
 func main() {
 
-	resultado := CalculoPolinomio(-6.0, 5.0, 4.0, 3.0, 2.0, 1.0)
+	//PuntoUno(-6.0, 5.0, 4.0, 3.0, 2.0, 1.0)
 
-	for _, coeficiente := range resultado {
-		fmt.Print(coeficiente)
-	}
-
-	fmt.Print("")
+	PuntoDos()
 
 }
 
-//By AgusLacomi
-/*
-	https://www.digitalocean.com/community/tutorials/an-introduction-to-the-strings-package-in-go-es
-*/
-func CalculoPolinomio(coeficientes ...float32) []string {
+/**
+ * @param coeficientes: Coeficientes de tipo float
+ * @pre: Se ingresa una cantidad indefinida de coeficientes necesariamente de tipo float
+ *		para armar el polinomio.
+ *
+ * @post: imprime por pantalla el polinomio formado con los parametros ingresados.
+ */
+func PuntoUno(coeficientes ...float32) {
 
 	arreglo := make([]string, len(coeficientes)) // make() crea un nuevo slice de strings
 	// con la cantidad de elementos igual a la cantidad de variables ingresadas
@@ -37,5 +36,29 @@ func CalculoPolinomio(coeficientes ...float32) []string {
 		}
 	}
 
-	return arreglo
+	for _, coeficiente := range arreglo {
+		fmt.Print(coeficiente)
+	}
+}
+
+/**
+ *
+ */
+func PuntoDos() {
+
+	for i := 1; i < 5; i++ {
+		fmt.Printf("- Opción %d\n", i)
+	}
+
+	var opcion int
+
+	fmt.Println("Seleccione el número PAR que sea MAYOR a 2")
+	fmt.Print("Opcion: ")
+	fmt.Scanln(&opcion)
+
+	if opcion == 4 {
+		fmt.Println("Opcion Correcta")
+	} else {
+		fmt.Println("Opcion Incorrecta")
+	}
 }
